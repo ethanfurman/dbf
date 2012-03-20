f7f6c33b9f766576ddec4144d9a1bae7e5091d6d
Ethan Furman <ethan@admailinc.com>
2012-03-20 09:51:53 -0700

    now supports null fields

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ import dbf
 html_docs = glob('dbf/html/*')
 
 long_desc="""
-Currently supports dBase III, and FoxPro - Visual FoxPro tables. Text is returned as unicode, and codepage settings in tables are honored. Documentation needs work, but author is very responsive to e-mails.
+Currently supports dBase III, and FoxPro - Visual FoxPro tables. Text is returned as unicode, and codepage settings in tables are honored. Null fields are now supported.  Documentation needs work, but author is very responsive to e-mails.
 
-Not supported: index files (but can create tempory non-file indexes), null fields (data returned is blank), and auto-incrementing fields.
-
+Not supported: index files (but can create tempory non-file indexes), and auto-incrementing fields.
 """
 
 setup( name='dbf',
-       version= '.'.join([str(x) for x in dbf.version]),
+       version= '%d.%02d.%03d' % dbf.version,
        license='BSD License',
        description='Pure python package for reading/writing dBase, FoxPro, and Visual FoxPro .dbf files (including memos)',
        long_description=long_desc,

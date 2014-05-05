bd0bdccae6292e6d07b4d8b5e2129c8b0e596ba1
Ethan Furman <ethan@stoneleaf.us>
2014-05-05 12:24:58 -0700

    add trove classifiers

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from distutils.core import setup
 from glob import glob
 import os
-import dbf
 
-html_docs = glob('dbf/html/*')
+#html_docs = glob('dbf/html/*')
 
 long_desc="""
 Currently supports dBase III, FoxPro, and Visual FoxPro tables. Text is returned as unicode, and codepage settings in tables are honored. Memos and Null fields are supported.  Documentation needs work, but author is very responsive to e-mails.
@@ -14,13 +13,14 @@ Latest release has many backwards incompatibilities, as well as some documentati
 """
 
 setup( name='dbf',
-       version= '%d.%02d.%03d' % dbf.version,
+       version= '0.95.012',
        license='BSD License',
        description='Pure python package for reading/writing dBase, FoxPro, and Visual FoxPro .dbf files (including memos)',
        long_description=long_desc,
        url='http://groups.google.com/group/python-dbase',
        py_modules=['dbf', 'dbf_test'],
        provides=['dbf'],
+       install_requires=['enum34'],
        author='Ethan Furman',
        author_email='ethan@stoneleaf.us',
        classifiers=[
@@ -28,6 +28,11 @@ setup( name='dbf',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
-            'Topic :: Database' ],
-     )
+            'Topic :: Database',
+            'Programming Language :: Python :: 2.4',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            ],
+    )
 

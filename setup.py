1c0b7135515f5135d95adf93346af364d0606b60
Ethan Furman <ethan@stoneleaf.us>
2015-08-14 11:31:02 -0700

    version 0.96.004

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 #html_docs = glob('dbf/html/*')
 
 long_desc="""
-Currently supports dBase III, FoxPro, and Visual FoxPro tables. Text is returned as unicode, and codepage settings in tables are honored. Memos and Null fields are supported.  Documentation needs work, but author is very responsive to e-mails.
+Currently supports dBase III, Clipper, FoxPro, and Visual FoxPro tables. Text is returned as unicode, and codepage settings in tables are honored. Memos and Null fields are supported.  Documentation needs work, but author is very responsive to e-mails.
 
 Not supported: index files (but can create tempory non-file indexes), auto-incrementing fields, and Varchar fields.
 
@@ -21,7 +21,7 @@ else:
     requirements = []
 
 setup( name='dbf',
-       version= '0.96.003',
+       version= '0.96.004',
        license='BSD License',
        description='Pure python package for reading/writing dBase, FoxPro, and Visual FoxPro .dbf files (including memos)',
        long_description=long_desc,

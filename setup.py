4120bbc4f2f15f363a575931431f805ecdf530ea
Ethan Furman <ethan@stoneleaf.us>
2023-12-07 16:37:01 -0800

    record support for python 2.7, 3.10, 3.11, and 3.12

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ data = dict(
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
             'Topic :: Database',
+            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
@@ -51,6 +52,9 @@ data = dict(
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: 3.12',
             ],
         )
 

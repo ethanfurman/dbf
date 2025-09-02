9a54ba56b28ae784257213f39fae9708e518b2a6
Ethan Furman <ethan@stoneleaf.us>
2025-09-02 13:07:09 -0700

    version 0.99.11

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ make = []
 
 data = dict(
         name='dbf',
-        version='0.99.11a1',
+        version='0.99.11',
         license='BSD License',
         description='Pure python package for reading/writing dBase, FoxPro, and Visual FoxPro .dbf files (including memos)',
         long_description=long_desc,
